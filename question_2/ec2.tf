f75553d072556854904922551adb36ffb3e30542
Tobi Adeniji <tobiadeniji@c-3p0.mynetworksettings.com>
2024-03-11 16:53:10 -0400

    minor change to keypair ref

--- question_2/ec2.tf
+++ question_2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web_instances" {
   count          = 3
   ami            = "ami-xxxxxxxxxxxxxxxx"  # Specify the desired AMI
   instance_type  = "t2.micro"  # Specify the desired instance type
-  key_name       = "your-key-pair-name"  # Specify your key pair name
+  key_name       = var.key_pair # Specify your key pair name
   security_group = [aws_security_group.web_sg.name]
 
   lifecycle {
